af0e7f38519083aada89d51054f4f66a71242f09
Ba Aly M'BAYE <baaly.mbaye@yahoo.fr>
2017-01-29 15:57:56 +0100

    Ajout entity Matiere

---
+++ SUPcourses/SUPcourses-ejb/src/java/com/supcourses/entity/Matiere.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.supcourses.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "matiere")
+public class Matiere implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @NotNull
+    @Column(name = "nom")
+    private String nom;
+
+    @NotNull
+    @Column(name = "description")
+    private String description;
+
+    @NotNull
+    @Column(name = "version", nullable = false, length = 50)
+    private String version;
+
+    @NotNull
+    @Column(name = "date_creation", nullable = false)
+    private Date dateCreation;
+
+    @NotNull
+    @Column(name = "auteur", nullable = false, length = 50)
+    private String auteur;
+
+    @NotNull
+    @Column(name = "quiz_id", nullable = false)
+    private int quizId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public Date getDateCreation() {
+        return dateCreation;
+    }
+
+    public void setDateCreation(Date dateCreation) {
+        this.dateCreation = dateCreation;
+    }
+
+    public String getAuteur() {
+        return auteur;
+    }
+
+    public void setAuteur(String auteur) {
+        this.auteur = auteur;
+    }
+
+    public int getQuizId() {
+        return quizId;
+    }
+
+    public void setQuizId(int quizId) {
+        this.quizId = quizId;
+    }
+
+}
